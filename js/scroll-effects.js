670ebfd92fc903f1e8e9e23077828aed69c9f539
lawrence <lawrence.t.yen@gmail.com>
2019-07-23 13:54:39 -0700

    added parallax

--- js/scroll-effects.js
+++ js/scroll-effects.js
@@ -1,17 +1,24 @@
 function parallaxFade() {
 	scrollPos = $(this).scrollTop();
-  docHeight = $(this).height();
-  sectionHeight = docHeight/4;
+  sectionHeight = $(this).height();
 
-  if (scrollPos < sectionHeight) {
-    $('#first-section').css({
+  if (scrollPos <= sectionHeight) {
+    $('#first-background').css({
       'opacity': 1 - (scrollPos / sectionHeight)
   	});
-  } else if (scrollPos > sectionHeight && scrollPos < 2 * sectionHeight)
-
-	$('#second-section').css({
-		'opacity': ((scrollPos - sectionHeight) / sectionHeight)
-	});
+  } else if (scrollPos >= sectionHeight && scrollPos < 2 * sectionHeight) {
+    $('#second-background').css({
+  		'opacity': 1 - ((scrollPos - sectionHeight) / sectionHeight)
+  	});
+  } else if (scrollPos >= 2 * sectionHeight && scrollPos < 3 * sectionHeight) {
+    $('#third-background').css({
+  		'opacity': 1 - ((scrollPos - 2 * sectionHeight) / sectionHeight)
+  	});
+  } else if (scrollPos >= 3 * sectionHeight) {
+    $('#fourth-background').css({
+  		'opacity': 1 - ((scrollPos - 3 * sectionHeight) / sectionHeight)
+  	});
+  }
 }
 $(document).ready(function(){
 	$(window).scroll(function() {
