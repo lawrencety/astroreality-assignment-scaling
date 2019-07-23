c234c131a7daaa59b8a61fdb875329311804d48a
lawrence <lawrence.t.yen@gmail.com>
2019-07-23 14:52:06 -0700

    added scaling

--- js/scroll-effects.js
+++ js/scroll-effects.js
@@ -1,10 +1,13 @@
 function parallaxFade() {
 	scrollPos = $(this).scrollTop();
   sectionHeight = $(this).height();
+  sectionWidth = $(this).width();
 
   if ((scrollPos <= sectionHeight / 2)) {
     $('#first-background').css({
-      'opacity': 1
+      'opacity': 1,
+      'height': sectionHeight - (sectionHeight * 0.05 * 2 * (scrollPos / sectionHeight)),
+      'width': sectionWidth - (sectionWidth * 0.05 * 2 * (scrollPos / sectionHeight)),
   	});
   } else if ((scrollPos >= sectionHeight / 2) && scrollPos <= sectionHeight) {
     $('#first-background').css({
@@ -15,7 +18,9 @@ function parallaxFade() {
       'opacity': 0
   	});
     $('#second-background').css({
-      'opacity': 1
+      'opacity': 1,
+      'height': 0.95 * sectionHeight - (sectionHeight * 0.05 * 2 * ((scrollPos - sectionHeight) / sectionHeight)),
+      'width': 0.95 * sectionWidth - (sectionWidth * 0.05 * 2 * ((scrollPos - sectionHeight)/ sectionHeight)),
   	});
   } else if (scrollPos >= 1.5 * sectionHeight && scrollPos <= 2 * sectionHeight) {
     $('#second-background').css({
@@ -26,7 +31,9 @@ function parallaxFade() {
       'opacity': 0
   	});
     $('#third-background').css({
-      'opacity': 1
+      'opacity': 1,
+      'height': 0.90 * sectionHeight - (sectionHeight * 0.05 * 2 * ((scrollPos - 2 * sectionHeight) / sectionHeight)),
+      'width': 0.90 * sectionWidth - (sectionWidth * 0.05 * 2 * ((scrollPos - 2 * sectionHeight)/ sectionHeight)),
   	});
   } else if (scrollPos >= 2.5 * sectionHeight && scrollPos <= 3 * sectionHeight) {
     $('#second-background').css({
@@ -40,7 +47,9 @@ function parallaxFade() {
       'opacity': 0
   	});
     $('#fourth-background').css({
-      'opacity': 1
+      'opacity': 1,
+      'height': 0.85 * sectionHeight - (sectionHeight * 0.05 * 2 * ((scrollPos - 3 * sectionHeight) / sectionHeight)),
+      'width': 0.85 * sectionWidth - (sectionWidth * 0.05 * 2 * ((scrollPos - 3 * sectionHeight)/ sectionHeight)),
   	});
   }
 }
